37093a00541c3f3c472bbf113384b3cf74ed245b
chovyBluecat <wanglonglong@vastai3d.com>
2025-04-26 22:50:49 +0800

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// 用于 GitHub Pages 部署，设置 base 路径（可根据仓库名修改）
-export default defineConfig({
-  plugins: [react()],
-  base: '/travis-blog/',
-});
