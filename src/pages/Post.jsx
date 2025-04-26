691da7ca47a8f2b22c531816a29137ee428b570d
chovyBluecat <wanglonglong@vastai3d.com>
2025-04-26 22:42:06 +0800

    fix:package.json

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import posts from '../posts';
+import posts from '../posts.jsx';
 
 export default function Post() {
   const { id } = useParams();
